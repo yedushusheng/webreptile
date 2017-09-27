3aa31684c30c8d1fc26bb453d0ea1e2fca61c44a
yedushusheng <yedushusheng@163.com>
2017-09-27 21:15:53 +0800

    利用request实现图片抓取

--- reptile.py
+++ reptile.py
@@ -1,9 +1,19 @@
-import urllib3
+#-*-coding:utf8-*-
 import re
-import urllib.request
+import requests
 
-req = urllib3.request.urlopen('http://www.imooc.com/course/list')
-buf = req.read()
-buf = buf.decode('utf-8')
-listurl = re.findall(r'src=.+\.jpg',buf)
-print(buf)
+#读取源代码文件
+f = open('G:\github\webreptile\source.txt','r')
+html = f.read()
+f.close()
+
+#匹配图片网址
+pic_url = re.findall('img src="(.*?)" class="lessonimg"',html,re.S)
+i = 0
+for each in pic_url:
+    print ('now downloading:' + each)
+    pic = requests.get(each)
+    fp = open('pic\\' + str(i) + '.jpg','wb')
+    fp.write(pic.content)
+    fp.close()
+    i += 1
